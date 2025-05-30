7fb0bab91404507553265e64e0d60f5ea6c4b3ee
Deepak Basavaraju <deepakbasavaraju@Deepaks-MacBook-Air.local>
2025-05-30 15:44:58 -0700

    bug fixes done. dmg created. working tested

--- setup.py
+++ setup.py
@@ -1,21 +1,37 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from dotenv import load_dotenv
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "modules"))
+# print("PYTHONPATH:", sys.path)
+
+load_dotenv()  # take environment variables from .env
 
 APP = ['JobRobo.py']
-DATA_FILES = []
+DATA_FILES = ['.env']  # Add any files you read from
 OPTIONS = {
     'argv_emulation': False,
-    'packages': ['tkinter', 'selenium', 'openai', 'pyautogui', 'pdfplumber', 'undetected_chromedriver'],
-    'includes': [],
-    'excludes': ['tqdm.tk', 'rubicon', 'typing_extensions', 'pypdfium2_raw', 'IPython', 'jupyter', 'mouseinfo', 'standalone', 'backports', 'backports.tarfile', 'notebook'],
-    'iconfile': 'assets/jobrobo.icns'
+    'packages': [
+        'tkinter', 'selenium', 'openai', 'pyautogui', 'pdfplumber', 'undetected_chromedriver',
+        'dotenv', 'modules'
+    ],
+    'includes': [
+        'runAiBot', 'modules.open_chrome'  # <-- explicitly include anything py2app might miss
+    ],
+    'excludes': [
+        'tqdm.tk', 'rubicon', 'typing_extensions', 'pypdfium2_raw',
+        'IPython', 'jupyter', 'mouseinfo', 'standalone', 'backports',
+        'backports.tarfile', 'notebook'
+    ],
+    'iconfile': 'assets/jobrobo.icns',
+    'resources': ['.env', 'assets']  # optional: include other needed resources
 }
-
-load_dotenv()  # take environment variables from .env.
 
 setup(
     app=APP,
-    data_files=DATA_FILES,
+    data_files=[],
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
+    packages=find_packages(),
 )
